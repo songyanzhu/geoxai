64af58febbaaa4844666cf48cda0703f54079329
Songyan Zhu <Songyan.Zhu@outlook.com>
2026-03-12 09:16:28 +0000

    Update __init__.py

--- geoxai/geo/__init__.py
+++ geoxai/geo/__init__.py
@@ -7,7 +7,7 @@ _data_path = Path(__file__).parent / "data"
 
 # Load csv
 world = gpd.read_file(_data_path / "world_naturalearth_lowres.geojson")
-world10m = gpd.read_file(_data_path / "geojson/WB_countries_Admin0_10m.json")
+world10m = gpd.read_file(_data_path / "geojson/WB_countries_Admin0_10m.geojson")
 
 # expose them as module variables
 globals().update(world)
