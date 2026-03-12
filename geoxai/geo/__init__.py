02d9055c48d7fd8ec701adfca7242d22677095bc
Songyan Zhu <Songyan.Zhu@outlook.com>
2026-03-12 09:14:39 +0000

    Create __init__.py

---
+++ geoxai/geo/__init__.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import geopandas as gpd
+from pathlib import Path
+
+# Path to the data folder
+_data_path = Path(__file__).parent / "data"
+
+# Load csv
+world = gpd.read_file(_data_path / "world_naturalearth_lowres.geojson")
+world10m = gpd.read_file(_data_path / "geojson/WB_countries_Admin0_10m.json")
+
+# expose them as module variables
+globals().update(world)
+globals().update(world10m)
+
+# control what `import *` exports
+__all__ = ["world", "world10m"]
